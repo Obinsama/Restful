8690213db9dc53b77af6e854a1fff16becf839e0
Obinsama <aubinsama@gmail.com>
2020-06-21 11:28:18 +0100

    edit create roles

--- resources/views/admin/roles/edit.blade.php
+++ resources/views/admin/roles/edit.blade.php
@@ -1,97 +1,99 @@
-@extends('layouts.app')
-
-
+@extends('admin.layout')
 @section('content')
 
-    <div class="row">
+    <br>
+    <br>
+    <br>
+    <div class="col-md-6 ml-auto mr-auto">
+        @if (count($errors) > 0)
 
-        <div class="col-lg-12 margin-tb">
+            <div class="alert alert-danger">
 
-            <div class="pull-left">
+                <strong>Whoops!</strong> There were some problems with your input.<br><br>
 
-                <h2>Edit Role</h2>
+                <ul>
+
+                    @foreach ($errors->all() as $error)
+
+                        <li>{{ $error }}</li>
+
+                    @endforeach
+
+                </ul>
 
             </div>
 
-            <div class="pull-right">
+        @endif
+        {{--            <div class="pull-right">--}}
 
-                <a class="btn btn-primary" href="{{ route('roles.index') }}"> Back</a>
+        {{--                <a class="btn btn-primary" style="float: right" href="{{ route('roles.index') }}"> Back</a>--}}
+
+        {{--            </div>--}}
+        {{--        <form  action="{{route('roles.update', $role->id)}}" method="PATCH" >--}}
+        {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
+        @csrf
+        <div class="card ">
+            <div class="card-header card-header-rose card-header-icon">
+                <div class="card-icon">
+                    <i class="material-icons">military_tech</i>
+                </div>
+                <h4 class="card-title">Create Role</h4>
 
             </div>
+            <div class="card-body ">
+                <div class="form-group bmd-form-group">
+                    <label for="exampleEmail" class="bmd-label-floating"> Nom </label>
+                    <input type="text" class="form-control" name="name" id="exampleEmail" required="true" aria-required="true" value="{{$role->name}} ">
+                </div>
+                {{--                    <div class="category form-category">* Required fields</div>--}}
+            </div>
+            <strong style="margin-left: 30px">Permission:</strong>
+            <div class="row" style="margin-left: 30px;">
 
+                <br>
+                <br>
+                <div class=" checkbox-radios" style="height:200px;column-count: 2">
+                    @foreach($permission as $value)
+                        <div class="form-check">
+                            <label class="form-check-label">{{ $value->name }}
+                                {{$check=in_array($value->id, $rolePermissions)}}
+                                @if($check)
+                                    <input name="permission[]" checked class="form-check-input" type="checkbox" value="{{$value->id}}"><span class="form-check-sign"><span class="check"></span></span></label>
+                            @else
+                                <input name="permission[]"  class="form-check-input" type="checkbox" value="{{$value->id}}"><span class="form-check-sign"><span class="check"></span></span></label>
+
+                            @endif
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+
+            {{--                <div class="form-group">--}}
+
+            {{--                    <strong>Permission:</strong>--}}
+
+            {{--                    <br/>--}}
+            {{--                    <div class="row" style="margin-left: 30px;">--}}
+            {{--                        <div class=" checkbox-radios" style="height:200px;column-count: 2">--}}
+            {{--                    @foreach($permission as $value)--}}
+            {{--                        <div class="form-check">--}}
+            {{--                        <label>{{ Form::checkbox('permission[]', $value->id, false, array('class' => 'form-check-input name')) }}--}}
+
+            {{--                            {{ $value->name }}</label>--}}
+            {{--                        </div>--}}
+            {{--                        <br/>--}}
+
+            {{--                    @endforeach--}}
+            {{--                        </div>--}}
+            {{--                    </div>--}}
+
+            {{--                </div>--}}
+
+            <div class="card-footer text-right">
+                <a href="{{route('roles.index')}}" class="btn btn-rose">Back</a>
+                <button type="submit" class="btn btn-rose">Create</button>
+            </div>
         </div>
-
+        </form>
     </div>
-
-
-    @if (count($errors) > 0)
-
-        <div class="alert alert-danger">
-
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-
-            <ul>
-
-                @foreach ($errors->all() as $error)
-
-                    <li>{{ $error }}</li>
-
-                @endforeach
-
-            </ul>
-
-        </div>
-
-    @endif
-
-
-    {!! Form::model($role, ['method' => 'PATCH','route' => ['roles.update', $role->id]]) !!}
-
-    <div class="row">
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <strong>Name:</strong>
-
-                {!! Form::text('name', null, array('placeholder' => 'Name','class' => 'form-control')) !!}
-
-            </div>
-
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12">
-
-            <div class="form-group">
-
-                <strong>Permission:</strong>
-
-                <br/>
-
-                @foreach($permission as $value)
-
-                    <label>{{ Form::checkbox('permission[]', $value->id, in_array($value->id, $rolePermissions) ? true : false, array('class' => 'name')) }}
-
-                        {{ $value->name }}</label>
-
-                    <br/>
-
-                @endforeach
-
-            </div>
-
-        </div>
-
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-
-        </div>
-
-    </div>
-
-    {!! Form::close() !!}
-
-
 @endsection
